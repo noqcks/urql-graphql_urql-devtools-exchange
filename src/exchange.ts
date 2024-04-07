81a4c8dd2cc53ec7c435f263e8635124dc122996
noqcks <benji@093b.org>
2024-04-07 02:32:29 +0000

    Upgrade dependencies

--- src/exchange.ts
+++ src/exchange.ts
@@ -150,7 +150,7 @@ export const devtoolsExchange = ((): Exchange => {
 
   // Prod or SSR
   if (process.env.NODE_ENV === 'production' || isSSR) {
-    return ({ forward }) => (ops$) => pipe(ops$, forward);
+    return ({ forward }) => (ops$) => pipe(ops$, forward(ops$));
   }
 
   if (isNative) {
